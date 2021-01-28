c0326af7d42879cd789932d46d9ffb6d0a78feff
ReneSena <rene.sena3005@gmail.com>
2021-01-27 22:38:02 -0300

    Criação do Quiz, utilizando os indices para controlar as perguntas.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import db from "../db.json";
 import Widget from "../src/components/Widget";
 import QuizBackground from "../src/components/QuizBackground";
+import QuizContainer from "../src/components/QuizContainer";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
 import Logo from "../src/components/QuizLogo";
@@ -11,18 +12,6 @@ import { Input } from "../src/components/Input";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { ListQuiz } from "../src/components/ListQuiz";
-
-export const QuizContainer = styled.div`
-	width: 100%;
-	max-width: 350px;
-	padding-top: 45px;
-	margin: auto 10%;
-
-	@media screen and (max-width: 500px) {
-		margin: auto;
-		padding: 15px;
-	}
-`;
 
 export default function Home() {
 	const router = useRouter();
@@ -41,7 +30,7 @@ export default function Home() {
 	return (
 		<QuizBackground backgroundImage={db.bg}>
 			<Head>
-				<title>Quiz - CDZ</title>
+				<title>{db.title}</title>
 			</Head>
 			<QuizContainer>
 				<Logo />
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,45 +1,139 @@
-import styled from "styled-components";
 import db from "../db.json";
 import Widget from "../src/components/Widget";
 import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
+import QuizContainer from "../src/components/QuizContainer";
 import GitHubCorner from "../src/components/GitHubCorner";
 import Logo from "../src/components/QuizLogo";
 import Head from "next/head";
 
-export const QuizContainer = styled.div`
-	width: 100%;
-	max-width: 350px;
-	padding-top: 45px;
-	margin: auto 10%;
+import { Button } from "../src/components/Button";
 
-	@media screen and (max-width: 500px) {
-		margin: auto;
-		padding: 15px;
-	}
-`;
+function LoadingWidget() {
+	return (
+		<>
+			<Widget>
+				<Widget.Header>Carregando...</Widget.Header>
+			</Widget>
+
+			<Widget>
+				<Widget.Content>[Loading da massa]</Widget.Content>
+			</Widget>
+		</>
+	);
+}
+
+function Questions(props) {
+	const { question, totalQuestions, questionIndex, onSubmit } = props;
+	const questionId = `question__${questionIndex}`;
+
+	return (
+		<Widget>
+			<Widget.Header>
+				<h3>
+					{`Pergunta ${questionIndex + 1}`} de {totalQuestions}
+				</h3>
+			</Widget.Header>
+
+			<img
+				alt="Descrição"
+				style={{
+					width: "100%",
+					objecFit: "cover",
+				}}
+				src={question.image}
+				height="200"
+			/>
+
+			<Widget.Content>
+				<h2>{question.title}</h2>
+				<p>{question.description}</p>
+
+				<form
+					onSubmit={(event) => {
+						event.preventDefault();
+
+						onSubmit();
+					}}
+				>
+					{question.alternatives.map(
+						(alternative, alternativeIndex) => {
+							const alternativeId = `alternative__${alternativeIndex}`;
+
+							return (
+								<Widget.Topic
+									key={alternativeIndex}
+									as="label"
+									htmlFor={alternativeId}
+								>
+									<input
+										id={alternativeId}
+										type="radio"
+										name={questionId}
+									/>
+									{alternative}
+								</Widget.Topic>
+							);
+						}
+					)}
+
+					<Button type="submit">Confirmar</Button>
+				</form>
+			</Widget.Content>
+		</Widget>
+	);
+}
+
+const screenStates = {
+	QUIZ: "QUIZ",
+	LOADING: "LOADING",
+	RESULT: "RESULT",
+};
 
 function Quiz() {
+	const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+	const totalQuestions = db.questions.length;
+	const [currentQuestion, setCurrentQuestion] = React.useState(0);
+	const questionIndex = currentQuestion;
+	const question = db.questions[questionIndex];
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			setScreenState(screenStates.QUIZ);
+		}, 1 * 1000);
+	}, []);
+
+	function handleSubmitQuiz() {
+		const nextQuestion = questionIndex + 1;
+
+		if (nextQuestion < totalQuestions) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setScreenState(screenStates.RESULT);
+		}
+	}
+
 	return (
 		<QuizBackground backgroundImage={db.bg}>
 			<Head>
-				<title>Quiz - CDZ</title>
+				<title>{db.title}</title>
 			</Head>
 			<QuizContainer>
 				<Logo />
-				<Widget>
-					<Widget.Header>
-						<h1>{db.title}</h1>
-					</Widget.Header>
-					<Widget.Content>Em construção</Widget.Content>
-				</Widget>
 
-				<Widget>
-					<Widget.Content>
-						<h1>Quizes da galera</h1>
-					</Widget.Content>
-				</Widget>
-				<Footer />
+				{screenState === screenStates.QUIZ && (
+					<Questions
+						question={question}
+						questionIndex={questionIndex}
+						totalQuestions={totalQuestions}
+						onSubmit={handleSubmitQuiz}
+					/>
+				)}
+
+				{screenState === screenStates.LOADING && <LoadingWidget />}
+
+				{screenState === screenStates.RESULT && (
+					<div>Você acertou X questões, parabéns!!</div>
+				)}
 			</QuizContainer>
 			<GitHubCorner projectUrl="https://github.com/ReneSena/quiz-cavaleiro-zodiaco" />
 		</QuizBackground>
