b66fad74ed4ffece56c9235a4a998c978b5d6b47
ReneSena <rene.sena3005@gmail.com>
2021-01-31 09:22:39 -0300

    Ajuste no script de gerar url dos quizes.

--- src/components/ListQuiz/index.js
+++ src/components/ListQuiz/index.js
@@ -9,12 +9,18 @@ export function ListQuiz(props) {
 	return (
 		<List>
 			{data.map((link) => {
+				let githubHost = link.includes('github.com');
+
 				const result = link.replace(
 					/\https:\/\/|\.vercel.app\/|\github.com\//g,
 					''
 				);
 
-				const [project, user] = result.split(/\.|\//g);
+				const urlOwner = githubHost
+					? result.split(/\.|\//g).reverse()
+					: result.split(/\.|\//g);
+
+				const [project, user] = urlOwner;
 
 				return (
 					<li key={link}>
